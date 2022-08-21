412884aee2882b877deec4460e1ec878eb6e1e79
n4o847 <22975590+n4o847@users.noreply.github.com>
2022-08-21 15:02:40 +0900

    バッテリーの電圧を測定（動作確認済み）

--- train_esp32_simple_with_battery_check/train_esp32_simple_with_battery_check.ino
+++ train_esp32_simple_with_battery_check/train_esp32_simple_with_battery_check.ino
@@ -1,9 +1,16 @@
+/*
+ * train_esp32_simple_with_battery_check
+ * 2022/08/21作成
+ * train_esp32_simpleにバッテリーの電圧を測る機能を付け加えたもの。
+ */
+
 #include <BluetoothSerial.h>
 BluetoothSerial SerialBT;
 
 /*------------------------------------------*/
-const int INPUT_PIN = A18; // モーターのピンGPIO25
-const int SENSOR_PIN = 4;  // ホールセンサーのピンGPIO4
+const int INPUT_PIN = A18;  // モーターのピンGPIO25
+const int SENSOR_PIN = 4;   // ホールセンサーのピンGPIO4
+const int BATTERY_PIN = 34; // バッテリーのピン
 int INPUT_MAX = 255;  // inputの上限
 int INPUT_MIN = 0;    // inputの下限
 int input = 0;  // モータへの入力(0～255)
@@ -12,11 +19,13 @@ int prev_sensor_value = LOW;
 /*---------------------------------------------*/
 
 void setup() {
-  SerialBT.begin("ESP32-Dr.");
+  SerialBT.begin("ESP32-Dr. 2");
   ledcSetup(0, 700, 8);
   ledcAttachPin(INPUT_PIN, 0);
+  analogSetAttenuation(ADC_6db);
   Serial.begin(115200);
-  pinMode(4, INPUT);
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(BATTERY_PIN, ANALOG);
 }
 
 void loop(){
@@ -34,4 +43,9 @@ void loop(){
     SerialBT.write('o');
     delay(3); //低速運転時のチャタリングを防止。
   }
+
+  // バッテリーの電圧(0～5000mV程度)を単位ミリボルトで得る
+  float battery = ((float) analogReadMilliVolts(BATTERY_PIN)) * 63.0 / 20.0 + 0.1;
+  Serial.print("battery: ");
+  Serial.println(battery);
 }
